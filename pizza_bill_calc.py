04b33864b64479a01b90a68526b12ff716f4664e
Yashada7 <118554169+Yashada7@users.noreply.github.com>
2022-11-26 15:16:12 +0530

    Update pizza_bill_calc.py

--- pizza_bill_calc.py
+++ pizza_bill_calc.py
@@ -1,13 +1,9 @@
 #Code by Yashada Tembe
 
-# 🚨 Don't change the code below 👇
 print("Welcome to Python Pizza Deliveries!")
 size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
-# 🚨 Don't change the code above 👆
-
-#Write your code below this line 👇
 
 if size=="S":
     if add_pepperoni=="Y":
