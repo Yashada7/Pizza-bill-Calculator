2d525c8711936f2e561159ae537b16ed17a3da28
Yashada7 <118554169+Yashada7@users.noreply.github.com>
2022-11-26 15:13:25 +0530

    Create pizza_bill_calc.py

---
+++ pizza_bill_calc.py
@@ -0,0 +1,45 @@
+#Code by Yashada Tembe
+
+# 🚨 Don't change the code below 👇
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+if size=="S":
+    if add_pepperoni=="Y":
+        if extra_cheese=="Y":
+            print("Your final bill is : $18.")
+        else:
+            print("Your final bill is : $17.")
+    else:
+        if extra_cheese=="Y":
+            print("Your final bill is : $16.")
+        else:
+            print("Your final bill is : $15.")
+elif size=="M":
+    if add_pepperoni=="Y":
+        if extra_cheese=="Y":
+            print("Your final bill is : $24.")
+        else:
+            print("Your final bill is : $23.")
+    else:
+        if extra_cheese=="Y":
+            print("Your final bill is : $21.")
+        else:
+            print("Your final bill is : $20.")
+else:
+    if add_pepperoni=="Y":
+        if extra_cheese=="Y":
+            print("Your final bill is : $29.")
+        else:
+            print("Your final bill is : $28.")
+    else:
+        if extra_cheese=="Y":
+            print("Your final bill is : $26.")
+        else:
+            print("Your final bill is : $25.")
+            
